66ac5df9b680b0cfaa18232ffad976bba85128f8
Sergio <sergiopdjr@hotmail.com>
2025-01-16 23:36:38 -0300

    configurando ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Teste from './Test.tsx'
+import EstiloGlobal from './styles.ts'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal/>
+      <Teste/>
+    </>
+  )
+}
+
+export default App
